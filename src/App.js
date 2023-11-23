95f7004f21ef83c8159b031dd05e833bfdd6bf3a
Wjascheslaw Prokhoda <wjascheslaw88@gmail.com>
2023-11-23 12:35:48 +0400

    Раскидал по компонентам

--- src/App.js
+++ src/App.js
@@ -1,65 +1,16 @@
 import React from 'react';
 import './App.css'
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Profile from './Components/Profile';
 
-function App() {
+
+const App = () => {
   return (
     <div className='app-wrapper'>
-
-        <header className='header'>
-          <img src="https://png.pngtree.com/template/20191024/ourmid/pngtree-mountain-landscape-logo-design-hiking-travel-and-adventure-concept-design-image_323034.jpg" alt=""/>
-        </header>
-
-        <nav className='nav'>
-
-          <div>
-            <a>Profile</a>
-          </div>
-
-          <div>
-            <a>Massages</a>
-          </div>
-
-          <div>
-            <a>News</a>
-          </div>
-
-          <div>
-            <a>Music</a>
-          </div>
-
-          <div>
-            <a>Setting</a>
-          </div>
-
-        </nav>
-
-        <div className='content'>
-          <div>
-          <img src='https://w.forfun.com/fetch/70/703e3aefd9500eff0f63294bc383ac2a.jpeg' alt=""/>
-          </div>
-
-          <div>
-            ava + description
-          </div>
-
-          <div>
-            my post
-            <div>
-              New post
-            </div>
-            <div>
-              <div>
-                post1
-              </div>
-              
-              <div>
-                post2
-              </div>
-            </div>
-          </div>
-
-        </div>
-
+        <Header />
+        <Nav />
+        <Profile />
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <header className='header'>
+        <img src="https://png.pngtree.com/template/20191024/ourmid/pngtree-mountain-landscape-logo-design-hiking-travel-and-adventure-concept-design-image_323034.jpg" alt=""/>
+      </header>
+    )
+}
+
+export default Header;
